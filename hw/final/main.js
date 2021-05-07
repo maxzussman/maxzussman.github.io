64a2cd4cb314dfec26d311f93e374e5c332af851
Max Zuss <maxzussman@gmail.com>
2021-05-07 12:39:31 -0400

    nicceee

--- hw/final/main.js
+++ hw/final/main.js
@@ -1,6 +1,20 @@
-$(document).ready(function() {
-  console.log("we loaded");
-  $("button").click(function() {
-    $("#footer").toggle();
-  });
-});
+const texts = ['the Corona Virus Away', 'the Jams Flowing', 'Your Mind at Ease', 'that Funky Soul'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector('.typing').textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 250);
+})();
